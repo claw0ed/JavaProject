c7a7a25ea71d8a0a38274fa88b5994056d234437
clae0ed <clae0ed@gmail.com>
2017-12-18 11:31:27 +0900

    2017.12.18

--- HelloJava/src/claw0ed/classes.java
+++ HelloJava/src/claw0ed/classes.java
@@ -4,8 +4,11 @@ public class classes {
 
 	public static void main(String[] args) {
 		// 클래스 - 객체를 만들기 위한 틀, 설계도
-		
-
+		Animal tiger = new Animal();
+		tiger.name = "호돌이";
+		tiger.weight = 100;
+		tiger.type = "호랑이";
+		tiger.gender = "남자";
 	}
 
 }
@@ -20,5 +23,4 @@ class Animal { // 동물 클래스
 	// 메서드
 	void move() { System.out.println(name + "가 움직였다"); }
 	void eat() { System.out.println(name + "가 먹이를 먹었다"); }
-	
 }
