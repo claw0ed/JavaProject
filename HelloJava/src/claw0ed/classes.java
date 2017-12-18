0723d6890075e3f8dd216ba64524274b687d6237
clae0ed <clae0ed@gmail.com>
2017-12-18 11:53:31 +0900

    2017.12.18

--- HelloJava/src/claw0ed/classes.java
+++ HelloJava/src/claw0ed/classes.java
@@ -9,6 +9,44 @@ public class classes {
 		tiger.weight = 100;
 		tiger.type = "호랑이";
 		tiger.gender = "남자";
+		
+		tiger.move();
+		tiger.eat();
+		
+		System.out.println(tiger.name);
+		System.out.println(tiger.weight);
+		System.out.println(tiger.type);
+		System.out.println(tiger.gender);
+		
+		// shark
+		Animal shark = new Animal();
+		shark.name = "상어가족";
+		shark.weight = 225;
+		shark.type = "상어";
+		shark.gender = "여자";
+		
+		shark.move();
+		shark.eat();
+		
+		System.out.println(shark.name);
+		System.out.println(shark.weight);
+		System.out.println(shark.type);
+		System.out.println(shark.gender);
+		
+		// eagle
+		Animal eagle = new Animal();
+		eagle.name = "독순이";
+		eagle.weight = 75;
+		eagle.type = "독수리";
+		eagle.gender = "여자";
+		
+		eagle.move();
+		eagle.eat();
+		
+		System.out.println(eagle.name);
+		System.out.println(eagle.weight);
+		System.out.println(eagle.type);
+		System.out.println(eagle.gender);
 	}
 
 }
@@ -24,3 +62,18 @@ class Animal { // 동물 클래스
 	void move() { System.out.println(name + "가 움직였다"); }
 	void eat() { System.out.println(name + "가 먹이를 먹었다"); }
 }
+
+class Car {
+	String name;
+	int size;
+	
+	// 클래스는 클래스의 멤버변수오 선언가능
+	Engin engin;
+	Door door;
+	Wheel wheel;
+	
+	
+}
+class Engin {}
+class Door {}
+class Wheel {}
