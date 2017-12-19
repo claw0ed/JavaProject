61f4ad2fd025828df60b7b197b6a34af2c0a6503
clae0ed <clae0ed@gmail.com>
2017-12-19 16:41:43 +0900

    2017.12.19

--- HelloJava/src/claw0ed/Inheritance.java
+++ HelloJava/src/claw0ed/Inheritance.java
@@ -3,15 +3,22 @@ package claw0ed;
 public class Inheritance {
 
 	public static void main(String[] args) {
-		// 
-
+		// 클래스간상속관계가 형성되면
+		// 각 클래스는 is-a 관계가 만들어진다
+		// 자식 클래스의 객체는 부모 클래스의 타입으로 처리 가능
+		Tiger t = new Tiger();
+		Animals t2 = new Tiger();
+		Animals e  = new Engle();
+		Animals g  = new Goldfish();
 	}
 
+	
 }
 
 class Animals {
-	String eye;
-	String mouth;
+	protected String eye; // 상속 관계에서는 protected 사용 (그래야 상속관계에서 사용할 수 있음)
+	protected String mouth;
+	protected String hp;
 	
 	public void eat() {
 		
@@ -23,7 +30,7 @@ class Animals {
 }
 
 class Tiger extends Animals{
-	String leg;
+	protected String leg;
 	
 	public void run() {
 		
@@ -31,7 +38,7 @@ class Tiger extends Animals{
 }
 
 class Engle extends Animals{
-	String wing;
+	protected String wing;
 	
 	public void fly() {
 		
@@ -39,7 +46,7 @@ class Engle extends Animals{
 }
 
 class Goldfish extends Animals{
-	String fin;
+	protected String fin;
 	
 	public void swim() {
 		
