cac3b24002d913e3afb3a9b5dfbda372de034bfa
clae0ed <clae0ed@gmail.com>
2017-12-19 14:35:59 +0900

    2017.12.19

--- HelloJava/src/claw0ed/lab/SungJukService.java
+++ HelloJava/src/claw0ed/lab/SungJukService.java
@@ -1,18 +1,49 @@
 package claw0ed.lab;
 
 public class SungJukService {
+	int tot = 0;
+	double avg = 0.0;
+	String grd = "가";
+	
 	// getTotal
 	public int getTotal(int kot, int eng, int mat) {
-		return (kot + eng + mat);
+//		return (kot + eng + mat);
+		tot = kot + eng + mat;
+		return tot;
+	}
+	
+	// 객체지향 특성 중 다형성
+	public int getTotal(SungJukV0 sj) {
+		tot = sj.getKor() + sj.getEng() + sj.getMat();
+		return tot;
 	}
 	
 	// getAverage
-	public double getAverage(int kot, int eng, int mat) {
-		return (kot + eng + mat) / 3;
+//	public double getAverage(int kot, int eng, int mat) {
+	public double getAverage() {
+//		return (kot + eng + mat) / 3;
+		avg = (double)tot / 3;
+		return avg;
 	}
 	
 	// getGrade
-	public int getGrade() {
-		return 0;
+	public String getGrade() {
+		switch((int)avg/10) {
+		case 10:
+		case 9:
+			grd = "수";
+			break;
+		case 8:
+			grd = "우";
+			break;
+		case 7:
+			grd = "미";
+			break;
+		case 6:
+			grd = "양";
+			break;
+		}
+		
+		return grd;
 	}
 }
