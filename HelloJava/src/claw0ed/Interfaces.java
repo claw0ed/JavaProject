8fbcb091e42bd6d7ee422955f698be22d1efa183
clae0ed <clae0ed@gmail.com>
2017-12-20 16:18:39 +0900

    2017.12.20

--- HelloJava/src/claw0ed/Interfaces.java
+++ HelloJava/src/claw0ed/Interfaces.java
@@ -3,15 +3,30 @@ package claw0ed;
 public class Interfaces {
 
 	public static void main(String[] args) {
-		// 인터페이스
+		// 인터페이스 - 객체화 불가 클래스
+		// 인터페이스를 구현한 클래스를 통해 객체화 가능
+		// ISungJukService isjsrv = new ISungJukService();
+		SungJukServiceImpl sjsrv = new SungJukServiceImpl();
 		
-
+		sjsrv.geTotal();
+		sjsrv.getAverage();
+		sjsrv.getGrade();
+		
+		// 추상 클래스 객체 생성가능?
+		// 추상 메서드를 포함한 클래스를 의미 - 객체화 불가!
+		// AbstractClass ac = new AbstractClass();
+		// 추상 클래스의 객체화는 상속을 통해 가능!
+		CompleteClass cc = new CompleteClass();
+		
 	}
 
 }
 
 interface ISungJukService {
-	void geTotal(); // 추상 매서드
+	int tot = 0; // 상수 (초기화 필요!), 인터페이스 변수가 아니라 상수라 초기화 필요
+	double avg = 0.0;
+	
+	abstract void geTotal(); // 추상 매서드라 abstract 사용하지 않아도 된다
 	
 	void getAverage(); // 추상 매서드
 	
@@ -23,20 +38,63 @@ class SungJukServiceImpl implements ISungJukService {
 
 	@Override
 	public void geTotal() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void getAverage() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void getGrade() {
-		// TODO Auto-generated method stub
 		
 	}
 
 }
+
+abstract class AbstractClass { // 추상 클래스
+	
+}
+
+class CompleteClass extends AbstractClass {
+	public CompleteClass() {
+		
+	}
+
+}
+
+class Unit {
+	
+}
+
+interface Fightable {
+	void move(int x, int y);
+	void attack(Unit u);
+	
+} // 미완성된 메서드 포함
+
+class Firebat implements Fightable {
+
+	@Override
+	public void move(int x, int y) {
+		
+	}
+
+	@Override
+	public void attack(Unit u) {
+		
+	}
+	
+} // 완전한 메서드 포함
+
+// 인터페이스를 구현 하는 중
+// 인터페이스의 모든 추상 메서드를 구현할 필요가 없으면
+// 추상 클래스로 만들면 된다!!
+abstract class Medic implements Fightable {
+	@Override
+	public void move(int x, int y) {
+		
+	}
+	
+} // 일부 완전한 메서드 포함
