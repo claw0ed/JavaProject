71b862e4721c4a72cdb5d6d5860490b707debaf3
clae0ed <clae0ed@gmail.com>
2017-12-21 16:06:13 +0900

    2017.12.21

--- HelloJava/src/claw0ed/Execptions.java
+++ HelloJava/src/claw0ed/Execptions.java
@@ -1,7 +1,7 @@
 package claw0ed;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Execptions {
 
@@ -21,21 +21,38 @@ public class Execptions {
 		}
 		
 		try {
-		int x = 1000 / 0;
+			// unchecked 예외 - 개발자가 직접 인지하고 예외 처리
+			int x = 1000 / 0;
 		} catch(Exception ex) {
 			System.out.println("0으로 나눌 수 없습니다");
 			System.out.println(ex.getMessage()); // 오류 관련정보
 			//ex.printStackTrace();
 		}
 		
-		// checked 예외
-		
+		// checked 예외 - 컴파일러가 미리 예외처리에 대한 정보를 알림
 		String fpath = "c:/Java/abc.dat";
 		try {
 			FileReader fr = new FileReader(fpath);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		} catch(Exception ex) {
+			System.out.println("파일관련 예외발생");
 		}
+		
+		// 예외처리 좋은 예
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("숫자만 입력하세요!");
+			try {
+				int key = sc.nextInt();
+				if(key == 0) break;
+			} catch(Exception ex) {
+				System.out.println("숫자관련 예외발생");
+				break;
+			}
+			
+		}
+		
+		sc.close();
 		
 		System.out.println("이글이 보이나요?");
 	}
