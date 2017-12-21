5c6a98bc3ede07c1aa3f9d8ba7d3408bf961475f
clae0ed <clae0ed@gmail.com>
2017-12-21 18:41:26 +0900

    2017.12.21

--- HelloJava/src/claw0ed/lab/Lab18.java
+++ HelloJava/src/claw0ed/lab/Lab18.java
@@ -4,7 +4,23 @@ public class Lab18 {
 
 	public static void main(String[] args) {
 		// 연습문제 7-7, 7-8, 7-9, 7-10, 7-15, 7-17, 8-2, 8-8
-
+		
+		// 7-15
+		class Unit {}
+		class AirUnit extends Unit {}
+		class GroundUnit extends Unit {}
+		class Tank extends GroundUnit {}
+		class AirCraft extends AirUnit {}
+		Unit u = new GroundUnit();
+		Tank t = new Tank();
+		AirCraft ac = new AirCraft();
+		
+		u = (Unit)ac;
+		u = ac;
+		GroundUnit gu = (GroundUnit)u;
+		AirUnit au = ac;
+		t = (Tank)u;
+		// GroundUnit gu = t;
 	}
 
 }
