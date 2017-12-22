f6d9fb8f73dd93dfde2dad0d70377c7f2d27a5a4
clae0ed <clae0ed@gmail.com>
2017-12-22 15:42:21 +0900

    2017.12.22

--- HelloJava/src/claw0ed/adv/ArrayLists.java
+++ HelloJava/src/claw0ed/adv/ArrayLists.java
@@ -1,12 +1,14 @@
 package claw0ed.adv;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import project.v1.SungJukV0;
 
 public class ArrayLists {
 
+	@SuppressWarnings("unchecked") // 워닝띄우지 마
 	public static void main(String[] args) {
 		// 배열
 		int[] intArray = new int[5];
@@ -36,7 +38,83 @@ public class ArrayLists {
 		double val2 = (double) list.get(2);
 		boolean val3 = (boolean) list.get(3);
 		SungJukV0 sj = (SungJukV0) list.get(4);
-
+		
+		// 지내릭스 Generics
+		// 컬렉션 프레임워크를 사용할때
+		// 타입을 저장하지 않고 객채를 저장하면
+		// 그것을 출력시 불필요한 형변환이 발생하는데
+		// JDK5에서는 이것을 막고자 지네릭을 도입
+		// 컬렉션을 선언할때 어떤 종류의 객체를 저장할지
+		// 미리 결정해야하는데 그것을 <> 기호로 정의
+		List cage1 = new ArrayList();
+		cage1.add(new Tiger());
+		cage1.add(new Zibra());
+		cage1.add(new Lion());
+		
+		Tiger t = (Tiger) cage1.get(0);
+		// Zibra z = (Tiger) cage1.get(1); // 안됨
+		// Lion l = (Zibra) cage1.get(2);
+		
+		List<Tiger> cage2 = new ArrayList<Tiger>();
+		cage2.add(new Tiger());
+		t = cage2.get(0);
+		// cage2.add(new Zibra()); // 다른 녀석들은 들어가지 않는다, Tiger만 들어감
+		
+		List<Zibra> cage3 = new ArrayList<>(); // JDK6부터는 <> 안넣고 줄여 쓸 수 있다
+		List<Lion> cage4 = new ArrayList<>();
+		
+		// ArrayList의 CRUD
+		// 추가 : add
+		List<String> names = new ArrayList<>();
+		names.add("혜교");
+		names.add("지연");
+		names.add("수지");
+		
+		// 조회 : get
+//		System.out.println(names.get(0));
+//		System.out.println(names.get(1));
+//		System.out.println(names.get(2));
+		System.out.println("for (int i = 0;i < names.size(); ++i)");
+		for (int i = 0;i < names.size(); ++i) {
+			System.out.println(names.get(i));
+		}
+		
+		System.out.println("for(String val : names)");
+		for(String val : names)
+			System.out.println(val);
+		
+		// 순환자를 이용해서 컬렉션의 모든 요소 출력
+		// 컬렉션클래스로 선언된 요소를
+		// 하나씩 읽어오기 위한 표준화된 방식을 제공 - 속도 느림 ㅠㅠ
+		// hasNext : 컬렉션 요소가 존재하는지 여부 검사
+		// next : 요소를 읽어 올수 있는지 여부 검사
+		// List는 향상된 for문 사용하는게 좋다
+		System.out.println("while(it.hasNext())");
+		Iterator<String> it = names.iterator();
+		while(it.hasNext()) {
+			String val = it.next();
+			System.out.println(val);
+		}
+		
+		// 갯수 : size
+		System.out.println("갯수 : size");
+		System.out.println(names.size());
+		
+		// 삭제 : remove(위치)
+		System.out.println("위치 : remove");
+		System.out.println(names.get(0));
+		names.set(0, "중기");
+		System.out.println(names.get(0));
+		
+		// 모두 제거 : clear
+		names.clear();
+		System.out.println("모두 제거 : clear");
+		System.out.println(names.size());
+		
 	}
 
 }
+
+class Tiger {}
+class Zibra {}
+class Lion {}
