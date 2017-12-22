5f40e6ef70feafbb630c607fb7ea462f92cc6e54
clae0ed <clae0ed@gmail.com>
2017-12-22 10:34:45 +0900

    2017.12.22

--- HelloJava/src/claw0ed/Exceptions2.java
+++ HelloJava/src/claw0ed/Exceptions2.java
@@ -1,7 +1,9 @@
 package claw0ed;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Exceptions2 {
 
@@ -16,8 +18,14 @@ public class Exceptions2 {
 			FileReader fr = new FileReader("");
 			
 			DriverManager.getConnection("");
-		} catch (Exception ex) {
-			System.out.println("시스템 오류 발생!");
+		} catch (ArrayIndexOutOfBoundsException aiob) {
+			System.out.println("잘못된 배열사용!");
+		} catch (ArithmeticException ae) {
+			System.out.println("잘못된 수식사용!");
+		} catch (FileNotFoundException ffe) {
+			System.out.println("파일경로를 제대로 설정하세요!");
+		} catch (SQLException se) {
+			System.out.println("데이터베이스 서버주소를 설정하세요!");
 		}
 	}
 
