46cbd1d1b04d414c554510215d2106c4b353993e
clae0ed <clae0ed@gmail.com>
2017-12-22 12:51:24 +0900

    2017.12.22

--- HelloJava/src/claw0ed/Strings.java
+++ HelloJava/src/claw0ed/Strings.java
@@ -10,9 +10,36 @@ public class Strings {
 		String n = "1234567890";
 		char c1 = s.charAt(1);
 		char c2 = n.charAt(6);
-				
+		
+		System.out.println("String (String s)");
 		System.out.println(c1);
 		System.out.println(c2);
+		
+		// String (char[] value)
+		char[] c = {'H', 'e', 'l', 'l', 'o'};
+		String s2 = new String(c);
+		
+		System.out.println("String (char[] value)");
+		System.out.println(s2);
+		
+		// String (StringBuffer buf)
+		StringBuffer sb = new StringBuffer("Hello");
+		String s3 = new String(sb);
+		
+		System.out.println("String (StringBuffer buf)");
+		System.out.println(sb);
+		System.out.println(s3);
+		
+		// char charAt (int index)
+		String s4 = "Hello";
+		String n4 = "0123456";
+		char c4 = s4.charAt(1);
+		char c5 = n4.charAt(1);
+		
+		System.out.println("char charAt (int index)");
+		System.out.println(c4);
+		System.out.println(c5);
+		
 	}
 
 }
