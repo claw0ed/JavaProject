fd5365b72ed92778e4be4ff72dfb4a4074c1b46b
clae0ed <clae0ed@gmail.com>
2017-12-26 16:55:54 +0900

    2017.12.26

--- HelloJava/src/claw0ed/adv/FileInputStreams.java
+++ HelloJava/src/claw0ed/adv/FileInputStreams.java
@@ -1,9 +1,33 @@
 package claw0ed.adv;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 public class FileInputStreams {
 
 	public static void main(String[] args) {
 		// 바이트기반 스트림
+		// 파일에 저장된 내용을 역직렬화를 통해 다시 객체로 변환
+		
+		String fpath = "c:/Java/data01.txt";
+		try(
+			FileInputStream fis = new FileInputStream(fpath); // 스트림 파일만 만들어줌
+			ObjectInputStream ois = new ObjectInputStream(fis); // 쓸려면 요거 있어야됨
+			) {
+			String name = (String)ois.readUTF(); // 직렬화로 문자형을 파일에서 읽어옴
+			Users u = (Users)ois.readObject(); // 직렬화로 문자형을 파일에서 읽어옴
+			int age = ois.readInt(); // 직렬화로 문자형을 파일에서 읽어옴
+			
+			System.out.println(name);
+			System.out.println(u.userid);
+			System.out.println(u.passwd);
+			System.out.println(u.name);
+			System.out.println(u.age);
+			
+			System.out.println(age);
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
 
 	}
 
--- HelloJava/src/claw0ed/adv/FileOutputStreams.java
+++ HelloJava/src/claw0ed/adv/FileOutputStreams.java
@@ -2,6 +2,7 @@ package claw0ed.adv;
 
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 public class FileOutputStreams {
 
@@ -10,14 +11,18 @@ public class FileOutputStreams {
 		// 객체를 바이트기반 스트림으로 파일에 저장
 		// 즉, 클래스에 대한 객체를 직렬화해서 파일에 저장함
 
+		String name = "지현";
 		Users u = new Users("zzyzzy", "123456", "혜교", 29);
+		int age = 57;
 		
 		String fpath = "c:/Java/data01.txt";
 		try(
-			FileOutputStream fos = new FileOutputStream(fpath);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			FileOutputStream fos = new FileOutputStream(fpath); // 스트림 파일만 만들어줌
+			ObjectOutputStream oos = new ObjectOutputStream(fos); // 쓸려면 요거 있어야됨
 			) {
+			oos.writeUTF(name); // 직렬화로 문자형을 파일에 저장
 			oos.writeObject(u); // 직렬화로 객체를 파일에 저장
+			oos.writeInt(age); // 직렬화로 숫자를 파일에 저장
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		}
@@ -25,7 +30,13 @@ public class FileOutputStreams {
 
 }
 
-class Users {
+// 클래스을 직렬화해서 파일에 저장하려면
+// 해당 클래스는 Serializable 인터패이스를 구현해야 함
+class Users implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4687321262437430313L;
 	String userid;
 	String passwd;
 	String name;
