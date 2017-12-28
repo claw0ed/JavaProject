a8cdd425ab9b41c94ec18899646e999f680b0bcb
clae0ed <clae0ed@gmail.com>
2017-12-28 15:36:06 +0900

    2017.12.28

---
+++ HelloJava/src/claw0ed/adv/MemberJDBC2.java
@@ -0,0 +1,101 @@
+package claw0ed.adv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class MemberJDBC2 {
+	
+	private static String DRV = "oracle.jdbc.driver.OracleDriver";
+	private static String URL = "jdbc:oracle:thin:@192.168.220.128:1521:xe";
+	private static String USR = "claw0ed";
+	private static String PWD = "123456";
+	
+	public static void main(String[] args) {
+		// JDBC 를 이용한 CRUD 예제
+		Connection conn = null; // DB 연결
+		PreparedStatement pstmt = null;  // DB SQL 문
+		ResultSet rs = null;    // select 결과 처리
+		
+		// 데이터 입력 받기
+		Scanner sc = new Scanner(System.in);
+		System.out.println("이름/주민번호/주소/전화번호 순으로 입력하세요");
+		String name = sc.nextLine();
+		String jumin = sc.nextLine();
+		String addr = sc.nextLine();
+		String hphone = sc.nextLine();
+		
+		try {
+			// 1. 데이터베이스 드라이버를 메모리에 올림
+			Class.forName(DRV);
+			// 2. 지정한 URL 로 데이터베이스 접속
+			conn = DriverManager.getConnection(URL, USR, PWD);
+			// 3. 실행할 SQL 문 작성
+			String sql = "insert into customer values "
+					+ "(cno.nextval, ?, ?, ?, ?)"; // ? : placeholder 실행속도면에서 Statement 보다 빠르다
+			pstmt = conn.prepareStatement(sql);
+			// placeholder에 실제 값 지정
+			pstmt.setString(1, name);
+			pstmt.setString(2, jumin);
+			pstmt.setString(3, addr);
+			pstmt.setString(4, hphone);
+			
+			// 4. 작성된 SQL 문을 실행
+			int cnt = pstmt.executeUpdate();
+			System.out.println(cnt + "건의 데이터가 추가됨!");
+			
+			// 3b. 실행할 SQL문을 작성
+			sql = "select * from customer order by cno desc";
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4b. SQL문을 실행하고 결과집합 저장
+			rs = pstmt.executeQuery(sql);
+			
+			// 4c. 결과 집합 처리
+			StringBuffer sb = new StringBuffer();
+			while(rs.next()) {
+				sb.append(rs.getString(1)).append(" ")
+				.append(rs.getString(2)).append(" ")
+				.append(rs.getString(3)).append(" ")
+				.append(rs.getString(4)).append(" ")
+				.append(rs.getString(5)).append("\n");
+			}
+			System.out.println(sb.toString());
+			
+//			while(rs.next()) { // StringBuffer 안사용
+//				System.out.print(rs.getString(1));
+//				System.out.print(rs.getString("cname"));
+//				System.out.print(rs.getString(3));
+//				System.out.print(rs.getString("addr"));
+//				System.out.println(rs.getString(5));
+//			}
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			// 5. 사용한 객체들을 모두 정리
+			try {
+				rs.close();
+			} catch (Exception ex) {
+
+			}
+			
+			try {
+				pstmt.close();
+			} catch (Exception ex) {
+
+			}
+			
+			try {
+				conn.close();
+			} catch (Exception ex) {
+				
+			}
+			
+		}
+		
+	}
+
+}
